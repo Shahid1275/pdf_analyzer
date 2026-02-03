2b738c542306b368361c23661812d57e3d8d28fd
Shahid Ameen <mern04@724.one>
2026-02-03 19:39:36 +0500

    done

--- backend/config/app.js
+++ backend/config/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+app.get('/', (req, res) => res.json({ message: 'Server is running' }));
 app.use('/api', pdfRoutes);
 
 // Health check
